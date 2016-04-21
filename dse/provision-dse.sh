8b84e3cd1f288e0797134492ef1e860381a8ab1a
Early Ehlinger <earlye@gmail.com>
2016-04-21 15:13:52 -0500

    CCP-5060 - stuff found during reintegration

--- dse/provision-dse.sh
+++ dse/provision-dse.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+DSE_VERSION=4.8.5-1
 sudo apt-get update
-sudo apt-get install dse-full=4.0.3-1 dse=4.0.3-1 dse-hive=4.0.3-1 dse-pig=4.0.3-1 dse-demos=4.0.3-1 dse-libsolr=4.0.3-1 dse-libtomcat=4.0.3-1 dse-libsqoop=4.0.3-1 dse-liblog4j=4.0.3-1 dse-libmahout=4.0.3-1 dse-libhadoop-native=4.0.3-1 dse-libcassandra=4.0.3-1 dse-libhive=4.0.3-1 dse-libpig=4.0.3-1 dse-libhadoop=4.0.3-1
+sudo apt-get install dse-full=$DSE_VERSION dse=$DSE_VERSION dse-hive=$DSE_VERSION dse-pig=$DSE_VERSION dse-demos=$DSE_VERSION dse-libsolr=$DSE_VERSION dse-libtomcat=$DSE_VERSION dse-libsqoop=$DSE_VERSION dse-liblog4j=$DSE_VERSION dse-libmahout=$DSE_VERSION dse-libhadoop-native=$DSE_VERSION dse-libcassandra=$DSE_VERSION dse-libhive=$DSE_VERSION dse-libpig=$DSE_VERSION dse-libhadoop=$DSE_VERSION
 
 sudo sed -i -e "s/start_rpc: false/start_rpc: true/g" /etc/dse/cassandra/cassandra.yaml
 sudo sed -i -e "s/rpc_address: localhost/rpc_address: 0.0.0.0/g" /etc/dse/cassandra/cassandra.yaml
